7572ebba712e60c8b6149909f3c4007038b6dffa
dbhan165 <dinhbaohan16052004@gmail.com>
2026-07-13 13:09:41 +0700

    fix: update Google OAuth Client ID for mcfh.io.vn

--- MCFH-Frontend/src/main.tsx
+++ MCFH-Frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* TODO: Thay chuỗi bên dưới bằng Client ID thực tế của dự án */}
-    <GoogleOAuthProvider clientId="223470488885-ne3j6eapo7hhfogo3cgvh17gero7ldbl.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="371030743086-5v6dfo6bgiaag8tergce1ca0gqr88ji7.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </StrictMode>,
