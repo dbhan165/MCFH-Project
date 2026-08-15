f80a03623f0942aa1922996da9132c47bc37c3f1
quin2401 <huydqde170488@fpt.edu.vn>
2026-08-15 14:00:24 +0700

    Fix: Add EF Core Migration for Mentions columns and fix Azure Blob connection string FormatException

--- MCFH/Migrations/20260815065921_UpdateMentionsColumns.Designer.cs
+++ MCFH/Migrations/20260815065921_UpdateMentionsColumns.Designer.cs
@@ -4,6 +4,7 @@ using MCFH.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MCFHBackend.Migrations
 {
     [DbContext(typeof(McfhDbContext))]
-    partial class McfhDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260815065921_UpdateMentionsColumns")]
+    partial class UpdateMentionsColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
